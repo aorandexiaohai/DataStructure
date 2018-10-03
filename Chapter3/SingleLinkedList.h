9c2cb999915f4821ff65d7066c4e20e51f589c0f
wzh <2663976081@qq.com>
2018-10-03 23:12:35 +0800

    实现SingleLinkedList 以及测试

---
+++ Chapter3/SingleLinkedList.h
@@ -0,0 +1,172 @@
+#pragma once
+#include <iomanip>
+
+namespace DS
+{
+template <class T>
+class SingleLinkedList
+{
+  private:
+    struct Node
+    {
+        T data = {};
+        Node *next = {};
+        ~Node()
+        {
+            delete this->next;
+        }
+        Node(const T &datapar) : data(datapar) {}
+    };
+    Node *head = {};
+    Node *tail = {};
+
+  public:
+    ~SingleLinkedList()
+    {
+        delete this->head;
+    }
+    bool Empty()
+    {
+        return head == nullptr;
+    }
+    void AddToHead(const T &data)
+    {
+        if (Empty())
+        {
+            head = new Node(data);
+            tail = head;
+        }
+        else
+        {
+            auto real_head = new Node(data);
+            real_head->next = head;
+            head = real_head;
+        }
+    }
+    void AddToTail(const T &data)
+    {
+        if (Empty())
+        {
+            head = new Node(data);
+            tail = head;
+        }
+        else
+        {
+            auto real_tail = new Node(data);
+            tail->next = real_tail;
+            tail = real_tail;
+        }
+    }
+    bool DeleteFromHead(T &result)
+    {
+        if (Empty())
+            return false;
+        else
+        {
+            auto result_node = head;
+            if (!head->next)
+            {
+                head = tail = nullptr;
+            }
+            else
+            {
+                head = head->next;
+            }
+            result = result_node->data;
+            //avoid recursive deletion
+            result_node->next = nullptr;
+            delete result_node;
+        }
+    }
+    bool DeleteFromTail(T &result)
+    {
+        if (Empty())
+            return false;
+        else
+        {
+            Node *pre_tail = head;
+            while (pre_tail && (pre_tail->next != tail))
+            {
+                pre_tail = pre_tail->next;
+            }
+            if (!pre_tail)
+            {
+                result = tail->data;
+                delete tail;
+                head = tail = nullptr;
+            }
+            else
+            {
+                pre_tail->next = nullptr;
+                result = tail->data;
+                delete tail;
+                tail = pre_tail;
+            }
+            return true;
+        }
+    }
+    bool DeleteElement(const T &ele)
+    {
+        Node *pre_node = nullptr;
+        Node *cur_node = nullptr;
+        if (ElementIsIn(ele, &pre_node, &cur_node))
+        {
+            auto next_node = cur_node->next;
+            if (pre_node)
+            {
+                pre_node->next = next_node;
+                if(!next_node)
+                {
+                    tail = pre_node;
+                }
+            }
+            else
+            {
+                if(!next_node)
+                {
+                    head = tail = nullptr;
+                }
+                else
+                {
+                    head = next_node;
+                }
+            }
+            cur_node->next = nullptr;
+            delete cur_node;
+            return true;
+        }
+        return false;
+    }
+    bool ElementIsIn(const T &ele, Node **pre_node_ptr = nullptr, Node **cur_node_ptr = nullptr)
+    {
+        if (pre_node_ptr)
+            *pre_node_ptr = nullptr;
+        if (cur_node_ptr)
+            *cur_node_ptr = nullptr;
+        auto tmp = head;
+        while (tmp && tmp->data != ele)
+        {
+            if (pre_node_ptr)
+                *pre_node_ptr = tmp;
+            tmp = tmp->next;
+        }
+        if (tmp)
+        {
+            if (cur_node_ptr)
+                *cur_node_ptr = tmp;
+            return true;
+        }
+        return false;
+    }
+    std::ostream &show(std::ostream &os, const std::string &spliter = " ")
+    {
+        auto tmp = head;
+        while (tmp)
+        {
+            os << tmp->data << spliter;
+            tmp = tmp->next;
+        }
+        return os;
+    }
+};
+} // namespace DS
---
+++ Chapter3/test_main.cpp
@@ -0,0 +1,58 @@
+#include "SingleLinkedList.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+const int TestSize = 10;
+const int TestCase = 100;
+const int TestRange = 100;
+#define SHOW_LIST(arr)        \
+    std::cout << #arr << ":"; \
+    arr.show(std::cout);      \
+    std::cout << std::endl;
+
+#define SHOW(value)             \
+    std::cout << #value << ":"; \
+    std::cout << value;         \
+    std::cout << std::endl;
+int main()
+{
+
+    std::srand(std::time(nullptr));
+
+    DS::SingleLinkedList<int> slli;
+    SHOW_LIST(slli);
+
+    for (int index = 0; index < TestCase; index++)
+    {
+        auto command = std::rand() % 5;
+        if (command == 0)
+        {
+            slli.AddToHead(std::rand() % TestRange);
+            SHOW_LIST(slli);
+        }
+        else if (command == 1)
+        {
+            auto delete_result = 0;
+            auto delete_state = slli.DeleteFromHead(delete_result);
+            SHOW_LIST(slli);
+        }
+        else if (command == 2)
+        {
+            slli.AddToTail(std::rand() % TestRange);
+            SHOW_LIST(slli);
+        }
+        else if (command == 3)
+        {
+            auto delete_result = 0;
+            auto delete_state = slli.DeleteFromTail(delete_result);
+            SHOW_LIST(slli);
+        }
+        else if (command == 4)
+        {
+            slli.DeleteElement(std::rand() % TestRange);
+            SHOW_LIST(slli);
+        }
+    }
+
+}
